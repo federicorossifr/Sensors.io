dbe9f6329bc388d46e51fca724c4457c5eb2c21f
Federico Rossi <f.rossi57@studenti.unipi.it>
2018-11-14 11:40:34 -0800

    Cleanup, added dependencies to package.json

--- origins/test.js
+++
@@ -1,9 +0,0 @@
-var request = require("request");
-
-
-var r = request.head("http://samplecsvs.s3.amazonaws.com/TechCrunchcontinentalUSA.csv");
-r.on("response",res=> {
-	console.log(res.headers);
-
-
-})
